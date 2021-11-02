7f16bf522b2004395ada9a0d87ab4ad57b7a77b7
Antoine CAILLOT <caillot@esigelec.fr>
2021-11-02 18:49:53 +0100

    Adding saving features

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,7 +4,8 @@ from carla import VehicleLightState as vls
 from tqdm import tqdm
 import random
 import time
-from typing import List
+import queue
+from typing import Any, List
 
 from utils.weatherList import *
 from utils.spawnClosestTo import *
@@ -17,12 +18,16 @@ DURATION = 15.0
 NFRAME = int(DURATION/TPS)+1 if DURATION%TPS > 0.5 else int(DURATION/TPS)
 WORLD = 'Town03'
 
+
 vehicle_spawn_pts = []
+embed_sens_t = Transform(Location(x=0, y=0, z=1.9)) # position of the sensors on the vehicle
 vehicle_spawn_pts.append(Transform(Location(x=4.503611, y=30.677336, z=1.495204), Rotation(pitch=3.871590, yaw=-82.755234, roll=-0.000122)))
 vehicle_spawn_pts.append(Transform(Location(x=1.972299, y=57.928772, z=1.107717), Rotation(pitch=1.554264, yaw=-85.449158, roll=-0.000122)))
 vehicle_spawn_pts.append(Transform(Location(x=-21.551464, y=7.175428, z=1.506268), Rotation(pitch=0.165127, yaw=54.112213, roll=-0.000122)))
-sensors_t = Transform(Location(x=0, y=0, z=13.0), Rotation(pitch=-20, yaw=90, roll=0)) # Position of the sensors of the infrastructure
-embed_sens_t = Transform(Location(x=0, y=0, z=1.9)) # position of the sensors on the vehicle
+
+
+infra_spawn_pts = [] # Position of the sensors of the infrastructure
+infra_spawn_pts.append(Transform(Location(x=0, y=0, z=13.0), Rotation(pitch=-20, yaw=90, roll=0)))
 
 
 def main():
@@ -35,13 +40,15 @@ def main():
 
     actor_list = []
     spawn_points = []
+    output_queue = queue.Queue()
+    FIRST_hjvgckfhxddts = True
 
     try:
         world = client.get_world()
         world = client.load_world(WORLD) # Setting the world
 
-        spectator = world.get_spectator()
-        spectator.set_transform(sensors_t)
+        # spectator = world.get_spectator()
+        # spectator.set_transform(sensors_t)
 
         traffic_manager = client.get_trafficmanager()
 
@@ -83,37 +90,50 @@ def main():
         camera_bp = get_KITTIcam_bp(blueprint_library)
         cameraSem_bp = get_KITTIcamSem_bp(blueprint_library)
 
-        v_attrib = Attribute(transform, blueprint, "output/")
+        v_attrib = Attribute(transform, blueprint, "output/V%03d/"%n)
         s_attrib = []
-        s_attrib.append(Attribute(sensors_t, camera_bp, "output/"))
-        s_attrib.append(Attribute(sensors_t, cameraSem_bp, "output/"))
-        agents.append(Agent("vehicle", n, v_attrib, s_attrib))
+        s_attrib.append(Attribute(embed_sens_t, camera_bp, "output/V%03d/"%n))
+        s_attrib.append(Attribute(embed_sens_t, cameraSem_bp, "output/V%03d/"%n))
+        agents.append(Agent("vehicle", n, "output/V%03d/"%n, v_attrib, s_attrib))
+
+    for n, transform in enumerate(infra_spawn_pts):
+        camera_bp = get_KITTIcam_bp(blueprint_library)
+        cameraSem_bp = get_KITTIcamSem_bp(blueprint_library)
+        s_attrib = []
+        s_attrib.append(Attribute(transform, camera_bp, "output/I%03d/"%n))
+        s_attrib.append(Attribute(transform, cameraSem_bp, "output/I%03d/"%n))
+        agents.append(Agent("infrastructure", n, "output/I%03d/"%n, None, s_attrib))
+
 
     
     for agent in agents:
-        actor_list = actor_list + agent.spawn(world)
+        actor_list = actor_list + agent.spawn(world, output_queue)
         print(agent.get_actors_list())
-
-    # # Vehicles' sensors
-    # V_sensors = []
-    # print(actor_list)
-    
-    # for i in range(len(vehicle_spawn_pts)):
-    #     V_sensors.append(spawnSensors(world, actor_list, "output/Embed/V%d"%i, embed_sens_t, world.get_actor(actor_list[i])))
-
-    # # Capteurs de l'infrastructure
-    # infra_sensors = spawnSensors(world, actor_list, 'output/Infra', sensors_t, None)
-    
-
 
     print("===========================\n\tRendering....\n===========================")
     try:
-        for _ in tqdm(range(NFRAME)):
-            world.tick()
+        for frame in tqdm(range(NFRAME)):
             actors = world.get_actors(actor_list)
-            for actor in actors:
-                pass
-                # print(actor.type_id.find("vehicle") != -1)
+
+            # print("Queue size : {}, empty : {}".format(output_queue.qsize(), output_queue.empty()))
+            snapshots = []
+            while not output_queue.empty():
+                snapshots.append(output_queue.get(True))
+
+            world.tick()
+            
+            if FIRST_hjvgckfhxddts:
+                FIRST_hjvgckfhxddts = False
+                time.sleep(2)
+
+            for (attrib, data) in snapshots:
+                attrib.save(data, frame)
+
+            for agent in agents:
+                agent.save_status(world, frame=frame)
+
+                # (attrib, data) = output_queue.clear()
+            #     attrib.save(data, frame)
             # time.sleep(0.2)
     finally:
         client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
--- utils/Agent.py
+++ utils/Agent.py
@@ -2,27 +2,64 @@ import carla
 from carla import Transform
 from carla import VehicleLightState as vls
 from typing import List
+import queue
+import numpy as np
+from math import pi, tan
+import json
+import os
 
 class Attribute:
-    def __init__(self, pose: Transform, bp, save_path: str):
+    def __init__(self, pose: Transform, bp, save_path: str, color_converter=carla.ColorConverter.Raw):
         self.pose = pose
         self.bp = bp
         self.save_path = save_path
         self.actor_id = None
         self.actor = None
-        if self.bp.id.find('vehicle') != -1:
-            self.type = 'v'
-        elif self.bp.id.find('sensor') != -1:
-            self.type = 's'
-        else:
-            self.type = 'u'
+        self.color_conv = color_converter
+        self.types = self.bp.id.split('.')
+        if self.types[0] == 'sensor':
+            self.save_path = self.save_path + self.types[1] + '_' + self.types[2] + '/'
+        if self.types[0] == 'vehicle':
+            self.save_path = self.save_path + "infos/"
+    
+    def spawn(self, world:carla.World, autopilot=None, attach_to=None, to_save:queue.Queue=None) -> None:
+        try:
+            actor = world.spawn_actor(self.bp, self.pose, attach_to=attach_to)
+            if autopilot != None:
+                actor.set_autopilot(autopilot)
+            if to_save != None:
+                actor.listen(lambda data: to_save.put((self, data)))
+            self.actor = actor
+            self.actor_id = actor.id
+        except Exception as e:
+            print("Exception spawning actor : {}".format(e))
 
-    def save(self, data: any, frame:int, color_converter=None):
-        if self.type == 's':
-            if color_converter == None:
-                data.save_to_disk(self.save_path%frame)
-            else:
-                data.save_to_disk(self.save_path%frame, color_converter)
+    def get_camera_matrix(self):
+        cameraFOV = self.bp.get_attribute('fov').as_float()
+        img_w = self.bp.get_attribute('image_size_x').as_int()
+        img_h = self.bp.get_attribute('image_size_y').as_int()
+        focal = img_w / (2 * tan(cameraFOV * pi / 360))
+        c_u = img_w / 2.0
+        c_v = img_h / 2.0
+        k = np.zeros((3,3), dtype=float)
+        k[0,0] = focal 
+        k[1,1] = focal
+        k[2,2] = 1.0
+        k[0,2] = c_u
+        k[1,2] = c_v
+        return k
+
+    def save(self, data: any, frame:int):
+        if self.types[1] == "camera":
+            data.save_to_disk(self.save_path+'%06d.png'%frame, color_converter=self.color_conv)
+            k = self.get_camera_matrix()
+            if not os.path.exists(self.save_path):
+                try:
+                    os.makedirs(self.save_path)
+                except OSError:
+                    print('Failed creating directory %s' % self.save_path)
+            if not os.path.exists('%s/cameraMatrix.npy'%self.save_path):
+                np.save('%s/cameraMatrix.npy'%self.save_path, k)
 
     def set_actor_id(self, actor_id):
         self.actor_id = actor_id
@@ -33,23 +70,23 @@ class Attribute:
     def get_actor(self):
         return self.actor
 
-    def spawn(self, world:carla.World, autopilot=None, attach_to=None) -> None:
-        try:
-            actor = world.spawn_actor(self.bp, self.pose, attach_to=attach_to)
-            if autopilot != None:
-                actor.set_autopilot(autopilot)
-            self.actor = actor
-            self.actor_id = actor.id
-        except Exception as e:
-            print("Exception spawning actor : {}".format(e))
-        
+    def get_transform(self):
+        return self.actor.get_transform().get_matrix()
+
+    def get_geoloc(self, world):
+        return world.get_map().transform_to_geolocation(self.actor.get_location())
+
+    def get_boundingbox(self):
+        return self.actor.bounding_box
+    
         
 class Agent:
-    def __init__(self, type: str, agentN, vehicle=None, sensors=None):
+    def __init__(self, type: str, agentN, path, vehicle=None, sensors=None):
         self.type = type
         self.agentN = agentN
         self.vehicle:Attribute = vehicle
         self.sensors:List[Attribute] = sensors
+        self.path = path
 
     def get_vehicle(self) -> Attribute:
         return self.vehicle
@@ -57,29 +94,59 @@ class Agent:
     def get_sensors(self) -> List[Attribute]:
         return self.sensors
 
-    def spawn(self, world) -> List[int]:
+    def spawn(self, world, saving_queue:queue.Queue=None) -> List[int]:
         parent=None
         actors_id:List[int] = []
         if self.vehicle != None:
             self.vehicle.spawn(world, autopilot=True)
             parent = self.vehicle.get_actor()
             actors_id.append(self.vehicle.get_actor_id())
-        for sensor in self.sensors:                                 # Bug potentiel. Est-ce une référence? Une copie de la variable? 
-            sensor.spawn(world, attach_to=parent)                   # Si bug -> fix avec un enumerate
-            # TODO listener pout placer dans une queue
+        for sensor in self.sensors:                                             # Bug potentiel. Est-ce une référence? Une copie de la variable? 
+            sensor.spawn(world, attach_to=parent, to_save=saving_queue)         # Si bug -> fix avec un enumerate
             actors_id.append(sensor.get_actor_id())
         return actors_id
 
     def get_actors_list(self) -> List[int]:
         out:List[int] = []
-        if self.vehicle.get_actor_id() != None:
-            out.append(self.vehicle.get_actor_id())
-        for sensor in self.sensors:
-            if sensor.get_actor_id() != None:
-                out.append(sensor.get_actor_id())
+        if self.vehicle != None:
+            if self.vehicle.get_actor_id() != None:
+                out.append(self.vehicle.get_actor_id())
+        if self.sensors != None:
+            for sensor in self.sensors:
+                if sensor.get_actor_id() != None:
+                    out.append(sensor.get_actor_id())
         return out
 
+    def save_status(self, world, frame):
+        vehicle = None
+        if self.vehicle != None:
+            Tv = self.vehicle.get_transform()
+            Lv = self.vehicle.get_geoloc(world)
+            BBox = self.vehicle.get_boundingbox()
+            vehicle = {"T_Mat": Tv, "loc": {"lat": Lv.latitude, "lon": Lv.longitude, "alt": Lv.altitude}, "BoundingBox": {"extent": {"x": BBox.extent.x,"y": BBox.extent.y, "z": BBox.extent.z}, "loc": {"x": BBox.location.x,"y": BBox.location.y, "z": BBox.location.z}, "rot": {"pitch": BBox.rotation.pitch,"yaw": BBox.rotation.yaw, "roll": BBox.rotation.roll}}}
+        
+        sensors = []
+        if self.sensors != None:
+            for n, s in enumerate(self.sensors):
+                T = s.get_transform()
+                sensors.append({'idx': n, "T_Mat": T})
 
+        
+        if vehicle != None:
+            out = {"vehicle": vehicle, "sensors": sensors}
+        else:
+            out = {"sensors": sensors}
+        
+        if not os.path.exists('%s/infos/'%self.path):
+            try:
+                os.makedirs('%s/infos/'%self.path)
+            except OSError:
+                print('Failed creating directory %s' % ('%s/infos/'%self.path))
+
+        f = open('%s/infos/%06d.json' % (self.path, frame), "w")
+        JDump = json.dumps(out)
+        f.write(JDump)
+        f.close()
 
 if __name__ == '__main__':
     test = Agent("None", 0)
